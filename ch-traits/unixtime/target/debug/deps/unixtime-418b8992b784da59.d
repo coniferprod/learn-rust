d956330cd88582214936e361ead36e9985d3a4d3
Jere Käpyaho <jere@coniferproductions.com>
2026-05-05 09:43:27 +0300

    Removed accidentally committed binaries

--- ch-traits/unixtime/target/debug/deps/unixtime-418b8992b784da59.d
+++
@@ -1,5 +0,0 @@
-/Users/jere/Projects/Rust/learn-rust/ch-traits/unixtime/target/debug/deps/unixtime-418b8992b784da59.d: src/main.rs
-
-/Users/jere/Projects/Rust/learn-rust/ch-traits/unixtime/target/debug/deps/unixtime-418b8992b784da59: src/main.rs
-
-src/main.rs:
--- ch-traits/unixtime/target/debug/unixtime.d
+++
@@ -1,1 +0,0 @@
-/Users/jere/Projects/Rust/learn-rust/ch-traits/unixtime/target/debug/unixtime: /Users/jere/Projects/Rust/learn-rust/ch-traits/unixtime/src/main.rs
