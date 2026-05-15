988be72a424c9947b769350cf589425ed42ad77c
Jere Käpyaho <2261936+jerekapyaho@users.noreply.github.com>
2026-05-15 11:42:06 +0300

    Added XML parsing example

---
+++ ch-xml/today/src/providers/mod.rs
@@ -0,0 +1,21 @@
+use crate::Event;
+use crate::filters::EventFilter;
+
+pub mod textfile;
+pub mod csvfile;
+pub mod sqlite;
+pub mod web;
+pub mod xmlfile;
+
+pub trait EventProvider {
+    fn name(&self) -> String;
+    fn get_events(&self, filter: &EventFilter, events: &mut Vec<Event>);
+    fn is_add_supported(&self) -> bool { false }
+    fn add_event(&self, event: &Event) -> Result<(), EventProviderError>;
+    fn kind(&self) -> String;
+}
+
+pub enum EventProviderError {
+    OperationNotSupported,
+    OperationFailed,
+}
