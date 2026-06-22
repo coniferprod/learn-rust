81ff93a350db02dad891aa0c9bb7196f70322e9b
Jere Käpyaho <2261936+jerekapyaho@users.noreply.github.com>
2026-06-22 11:36:57 +0300

    Updated error example

--- ch-errors/src/main.rs
+++ ch-errors/src/main.rs
@@ -1,15 +1,32 @@
 use std::fs::File;
+use std::io::{self, Read};
 
-fn main() {
-    let file_result = File::open("events.txt");
+fn read_events(filename: &str) -> Result<String, io::Error> {
+    let file_result = File::open(filename);
 
-    let file = match file_result {
-        Ok(f) => f,
-        Err(e) => {
-            eprintln!("Error opening file: {e}");
-            return;
-        }
+    let mut file = match file_result {
+        Ok(file) => file,
+        Err(e) => return Err(e),  // propagate the error (1)
     };
 
-    println!("File was found, OK to start reading.");
+    // OK, we have a file, let's read its content:
+
+    let mut text = String::new();
+    match file.read_to_string(&mut text) {
+        Ok(_) => Ok(text),
+        Err(e) => Err(e),  // propagate the error (2)
+    }
+}
+
+fn main() {
+    let filename = "events.txt";
+    match read_events(&filename) {
+        Ok(text) => {
+            println!("{}", text);
+        },
+        Err(e) => {
+            eprintln!("Unable to read from file '{}', error: {}", 
+                filename, e);
+        }
+    }
 }
